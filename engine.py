46fd7f0da16211611222e75fba295ceb8ab3e5b2
Hongyu Su <hongyu.su@me.com>
2018-02-13 23:38:12 +0200

    update

--- engine.py
+++ engine.py
@@ -27,8 +27,7 @@ PRODUCT = 'BTC-EUR'
 
 # global variables 
 euro_pool = LIMIT_EURO 
-price_pool = [10e10]*10
-order_book = []
+price_pool = [0]*10
 
 
 #def log_current_status(current_price): 
@@ -54,8 +53,16 @@ order_book = []
 
 def buy_order_from_pp():
     global price_pool
+    logger.info("<--- {:.2f} --  {} -- {:.2f} -- {} -- {:.2f} -- {} -- {:.2f}".format(
+        price_pool[-1], 
+        price_pool[-1] < price_pool[-2], 
+        price_pool[-2], 
+        price_pool[-2] < price_pool[-3], 
+        price_pool[-3], 
+        price_pool[-3] < price_pool[-4], 
+        price_pool[-4]))
     try:
-        if price_pool[-1] < price_pool[2] and price_pool[-2] < price_pool[-3]:
+        if price_pool[-1] < price_pool[-2] and price_pool[-2] < price_pool[-3] and price_pool[-3] < price_pool[-4]:
             return True
         else:
             return False
@@ -66,25 +73,21 @@ def buy_order_from_pp():
 
 def generate_buy_order(ac, pc, n):
 
-    global price_pool
-    global order_book
-
     try:
-#        ask_price_list = pc.get_product_order_book(PRODUCT,level=1)['asks']
-#        price_to_buy, _, _ = ask_price_list[0]
-#        price_to_buy = eval(price_to_buy)
-        price_to_buy = list(range(100))[-n-1]
+        ask_price_list = pc.get_product_order_book(PRODUCT,level=1)['asks']
+        price_to_buy, _, _ = ask_price_list[0]
+        price_to_buy = eval(price_to_buy)
+        #price_to_buy = list(range(100))[-n-1]
         price_pool.append(price_to_buy)
 
         if buy_order_from_pp():
-            price_to_buy -= 0.01
+            price_to_buy -= 0.05
             euro_cost = price_to_buy * LIMIT_VOLUME 
-            euro_pool = eval(ac.get_account('e1ce1c04-208f-4e18-8062-52961c9c7eb7')['balance'])
+            euro_pool = eval(ac.get_account('e1ce1c04-208f-4e18-8062-52961c9c7eb7')['balance']) - 70
             if euro_cost < euro_pool:
                 # buy
-                #buy_order = ac.buy(price='{}'.format(price_to_buy), size=LIMIT_VOLUME,product_id=PRODUCT)
-                order = {'id':'53a88d80-f55a-4ea7-a3b9-946f79b9685f'}
-                order_book.append(order['id'])
+                buy_order = ac.buy(price='{}'.format(price_to_buy), size=LIMIT_VOLUME,product_id=PRODUCT)
+                #buy_order = {'id':'53a88d80-f55a-4ea7-a3b9-946f79b9685f'}
                 logger.info('{:6s} ORDER AT {:.8f} WITH {:.8f}'.format('BUY', price_to_buy, euro_pool))
 
     except Exception as e:
@@ -93,21 +96,29 @@ def generate_buy_order(ac, pc, n):
         logger.critical('--------------------')
 
 def generate_sell_order(ac, pc):
-    global order_book
 
-    if len(order_book) == 0:
-        return
     fills = ac.get_fills()
     try:
+        buy = 0
+        sell = 0
         for order in fills[0]:
-            if order['order_id'] == order_book[-1]:
-
+            if order['side'] == 'buy':
+                buy += 1
+            if order['side'] == 'sell':
+                sell += 1
+        n = buy - sell
+        if n == 0:
+            return
+        for order in fills[0]:
+            if order['side'] == 'buy':
                 price_to_sell = eval(order['price']) + 10
                 # sell
-                sell_order = ac.sell(price='{}'.format(price_to_sell),size=LIMIT_VOLUME,product_id=PRODUCT)
+                sell_order = ac.sell(price='{}'.format(price_to_sell),size='{}'.format(LIMIT_VOLUME),product_id=PRODUCT)
                 if 'id' in sell_order:
-                    order_bool.remove(order_book[-1])
                     logger.info('{:6s} ORDER AT {:.8f} WITH {:.8f}'.format('SELL', price_to_sell, euro_pool))
+            n -= 1
+            if n == 0:break
+
     except Exception as e:
         logger.critical('--------------------')
         traceback.print_exc()
