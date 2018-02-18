b52d16c279c2f243a4ab8aa0d72fcc65222d325f
Hongyu Su <hongyu.su@me.com>
2018-02-18 16:37:45 +0200

    support trading multiple currencies given configuration

--- engine.py
+++ engine.py
@@ -9,42 +9,48 @@ import logging
 import traceback
 import time
 import sys
+import utils.configurationProvider as cp
 from time import time
 from time import sleep
 import re
 import gdax
 import random
 
+configs = cp.get_configs()
+API_KEY          = configs.get("API_KEY")
+API_SECRET       = configs.get("API_SECRET")
+API_PASSPHRASE   = configs.get("API_PASSPHRASE")
+EURO_ACCOUNT     = configs.get("EURO_ACCOUNT")
+LIMIT_EURO       = configs.get("LIMIT_EURO")
+LIMIT_VOLUME     = configs.get("LIMIT_VOLUME")
+PRODUCT_NAME     = configs.get("PRODUCT_NAME")
+DELTA_BUY_PRICE  = configs.get("DELTA_BUY_PRICE")
+DELTA_SELL_PRICE = configs.get("DELTA_SELL_PRICE")
+
 logging.basicConfig(format='%(asctime)s %(name)15s %(levelname)10s:%(message)s')
 logger = logging.getLogger('gdax.engine')
 logger.setLevel(logging.INFO)
 logger.setLevel(logging.DEBUG)
 
-# TODO: read from configuration file
-LIMIT_EURO = 10
-LIMIT_VOLUME = 1e-3 
-PRODUCT = 'BTC-EUR'
-DELTA_BUY_PRICE = 15
-DELTA_SELL_PRICE = 50
+logger.info("{}".format('-'*40 + 'CONFIGURATION' + '-'*40))
+logger.info("{:35} {}".format("API_KEY", API_KEY))
+logger.info("{:35} {}".format("API_SECRET", API_SECRET))
+logger.info("{:35} {}".format("API_PASSPHRASE", API_PASSPHRASE))
+logger.info("{:35} {}".format("EURO_ACCOUNT", EURO_ACCOUNT))
+logger.info("{:35} {}".format("LIMIT_EURO", LIMIT_EURO))
+logger.info("{:35} {}".format("LIMIT_VOLUME", LIMIT_VOLUME))
+logger.info("{:35} {}".format("PRODUCT_NAME", PRODUCT_NAME))
+logger.info("{:35} {}".format("DELTA_BUY_PRICE", DELTA_BUY_PRICE))
+logger.info("{:35} {}".format("DELTA_SELL_PRICE", DELTA_SELL_PRICE))
+logger.info("{}".format('-'*93))
 
 # global variables 
 price_pool = [0]*10
 
 
-# initialization
-configs = {}
-for line in open('./utils/configs_prod.ini'):
-    if line.startswith('APIKEY'):
-        APIKEY = re.sub('^APIKEY=','',line.strip())
-    if line.startswith('APISECRET'):
-        APISECRET = re.sub('^APISECRET=','',line.strip())
-    if line.startswith('PASSPHRASE'):
-        PASSPHRASE = re.sub('^PASSPHRASE=','',line.strip())
-
-# public client
+# clients 
 pc = gdax.PublicClient()
-# authorized client
-ac = gdax.AuthenticatedClient(APIKEY, APISECRET, PASSPHRASE)
+ac = gdax.AuthenticatedClient(API_KEY, API_SECRET, API_PASSPHRASE)
 
 # all buy order 
 pending_buy_order = {} 
@@ -57,13 +63,13 @@ last_buy_price = 0
 
 def buy_order_from_pp():
     global price_pool
-    logger.debug("<--- {:8.2f} --  {} -- {:8.2f} -- {} -- {:8.2f} -- {} -- {:8.2f}".format(
+    logger.debug("<--- {:8.2f} --  {:6s} -- {:8.2f} -- {:6s} -- {:8.2f} -- {:.6s} -- {:8.2f}".format(
         price_pool[-1], 
-        price_pool[-1] < price_pool[-2], 
+        str(price_pool[-1] < price_pool[-2]), 
         price_pool[-2], 
-        price_pool[-2] < price_pool[-3], 
+        str(price_pool[-2] < price_pool[-3]), 
         price_pool[-3], 
-        price_pool[-3] < price_pool[-4], 
+        str(price_pool[-3] < price_pool[-4]), 
         price_pool[-4]))
 
     try:
@@ -84,21 +90,21 @@ def generate_buy_order(n):
 
     try:
         sleep(0.5)
-        ask_price_list = pc.get_product_order_book(PRODUCT,level=1)['asks']
+        ask_price_list = pc.get_product_order_book(PRODUCT_NAME,level=1)['asks']
         price_to_buy, _, _ = ask_price_list[0]
         price_to_buy = eval(price_to_buy)
         price_pool.append(price_to_buy)
         price_to_buy -= 0.1
 
-        if abs(last_buy_price - price_to_buy) > DELTA_BUY_PRICE and buy_order_from_pp():
+        if buy_order_from_pp() and abs(last_buy_price - price_to_buy) > DELTA_BUY_PRICE:
             euro_cost = price_to_buy * LIMIT_VOLUME 
-            account_info = ac.get_account('e1ce1c04-208f-4e18-8062-52961c9c7eb7')
-            if euro_cost < account_info['available'] - LIMIT_EURO:
+            account_info = ac.get_account(EURO_ACCOUNT)
+            if euro_cost < eval(account_info['available']) - LIMIT_EURO:
                 # buy
-                buy_order = ac.buy(price='{}'.format(price_to_buy), size=LIMIT_VOLUME,product_id=PRODUCT)
+                buy_order = ac.buy(price='{}'.format(price_to_buy), size=LIMIT_VOLUME,product_id=PRODUCT_NAME)
                 print(buy_order)
                 pending_buy_order[buy_order['id']] = price_to_buy
-                logger.info('{:6s} ORDER AT {:.8f} WHEN BALANCE {:.8f} AVAILABLE {:.8f} HOLD {:.8f}'.format('BUY', price_to_buy, account_info['balance'], account_info['available'], account_info['hold']))
+                logger.info('{:6s} ORDER AT {:.8f} WHEN BALANCE {} AVAILABLE {} HOLD {}'.format('BUY', price_to_buy, account_info['balance'], account_info['available'], account_info['hold']))
                 last_buy_price = price_to_buy
                 price_pool.append(-1)
                 sleep(3)
@@ -117,7 +123,7 @@ def generate_sell_order():
             price_to_sell += DELTA_SELL_PRICE
             order_info = ac.get_fills(order_id=pending_buy_order_id)[0]
             if len(order_info) > 0:
-                sell_order = ac.sell(price='{:.2f}'.format(price_to_sell),size='{}'.format(LIMIT_VOLUME),product_id=PRODUCT)
+                sell_order = ac.sell(price='{:.2f}'.format(price_to_sell),size='{}'.format(LIMIT_VOLUME),product_id=PRODUCT_NAME)
                 print(sell_order)
                 to_remove.append(pending_buy_order_id)
                 logger.info('{:6s} ORDER AT {:.8f}'.format('SELL', price_to_sell))
---
+++ utils/configurationProvider.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------+
+#                                                 |
+# trading                                         |
+#                                                 |
+#                                                 |
+# ------------------------------------------------+
+import configparser
+import os
+config = configparser.ConfigParser()
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+default_path = os.path.join(__location__, 'configs/config.ini')
+
+CONFIG_FILE = os.environ.get('CONFIG_FILE', default_path)
+
+config.read(CONFIG_FILE)
+
+
+def get_configs():
+    # GDAX_ENV can be set to DEFAULT, LOCAL or DOCKER now
+
+    currentenvironment = os.environ.get('GDAX_ENV','DEFAULT')
+    params = config[currentenvironment.upper()]
+
+    API_KEY          = os.environ.get('API_KEY',params.get('API_KEY'))
+    API_SECRET       = os.environ.get('API_SECRET',params.get('API_SECRET'))
+    API_PASSPHRASE   = os.environ.get('API_PASSPHRASE',params.get('API_PASSPHRASE'))
+    LIMIT_EURO       = os.environ.get('LIMIT_EURO',params.get('LIMIT_EURO'))
+    LIMIT_VOLUME     = os.environ.get('LIMIT_VOLUME',params.get('LIMIT_VOLUME'))
+    PRODUCT_NAME     = os.environ.get('PRODUCT_NAME',params.get('PRODUCT_NAME'))
+    DELTA_BUY_PRICE  = os.environ.get('DELTA_BUY_PRICE',params.get('DELTA_BUY_PRICE'))
+    DELTA_SELL_PRICE = os.environ.get('DELTA_SELL_PRICE',params.get('DELTA_SELL_PRICE'))
+    EURO_ACCOUNT     = os.environ.get('EURO_ACCOUNT',params.get('EURO_ACCOUNT'))
+
+    return {
+        "API_KEY":          API_KEY,
+        "API_SECRET":       API_SECRET,
+        "API_PASSPHRASE":   API_PASSPHRASE,
+        "LIMIT_EURO":       float(LIMIT_EURO),
+        "LIMIT_VOLUME":     float(LIMIT_VOLUME),
+        "PRODUCT_NAME":     PRODUCT_NAME,
+        "DELTA_BUY_PRICE":  float(DELTA_BUY_PRICE),
+        "DELTA_SELL_PRICE": float(DELTA_SELL_PRICE),
+        "EURO_ACCOUNT":     EURO_ACCOUNT,
+    }
